341ffe1fcbbc15ff72c05c10284f3b2cba3f9b9b
Miller Andres Rocha Castano <miller.rocha@globant.com>
2022-03-24 14:38:30 -0500

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 const getForecast = function(location){
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
         .then((response)=>{
             response.json().
                 then((data)=>{
